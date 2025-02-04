86f223634296ef90403db601211d2fb33a1ae3f7
Aman Bhaskar <122169164+Bhaskar0320@users.noreply.github.com>
2025-02-04 16:58:03 +0530

    line 16-20 appconfig

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -8,13 +8,19 @@ export const API_BASE_URL = window.location.hostname === "localhost"
   ? "http://localhost:5454" 
   : "https://ecomserver-bhasjar.up.railway.app";
 
-const jwt = localStorage.getItem("jwt")
+const token = localStorage.getItem("jwt")
 
 
 export const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers:{
-    "Authorization":`Bearer ${jwt}`,
-    "Content-Type": "application/json"
-  }
+  baseURL: API_BASE_URL
+  // headers:{
+  //   "Authorization":`Bearer ${token}`,
+  //   "Content-Type": "application/json"
+  // }
 })
+
+api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+api.defaults.headers.post['Content-Type'] = 'application/json';
+
+
