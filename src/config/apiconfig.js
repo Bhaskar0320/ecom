9cbfa0299360f69986da0c2fcac4c3187bf7aed6
Aman Bhaskar <122169164+Bhaskar0320@users.noreply.github.com>
2025-02-05 01:57:13 +0530

    restored 16 - 19 line action auth

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -8,19 +8,19 @@ export const API_BASE_URL = window.location.hostname === "localhost"
   ? "http://localhost:5454" 
   : "https://ecomserver-bhasjar.up.railway.app";
 
-const jwt = localStorage.getItem("jwt")
+const token = localStorage.getItem("jwt")
 
 
 export const api = axios.create({
   baseURL: API_BASE_URL
-  // headers:{
-  //   "Authorization":`Bearer ${token}`,
-  //   "Content-Type": "application/json"
-  // }
+  headers:{
+    "Authorization":`Bearer ${token}`,
+    "Content-Type": "application/json"
+  }
 })
 
-api.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
+// api.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
 
-api.defaults.headers.post['Content-Type'] = 'application/json';
+// api.defaults.headers.post['Content-Type'] = 'application/json';
 
 
