c6534705c0846846c1edabbca5a3e0cd403cf3a9
Aman Bhaskar <122169164+Bhaskar0320@users.noreply.github.com>
2025-02-05 05:41:57 +0530

    added page size

--- src/State/Product/Action.js
+++ src/State/Product/Action.js
@@ -33,7 +33,7 @@ export const findProducts = (reqData) => async (dispatch) => {
     // console.log("product data", data)
 
     await api.get(`/api/products?color=${colors}&size=${sizes}&minPrice=0&maxPrice=10000
-  &minDiscount=10&category=${category}&stock=${stock}&sort=${sort}&pageNumber=0&pageSize=0`);
+  &minDiscount=10&category=${category}&stock=${stock}&sort=${sort}&pageNumber=0&pageSize=12`);
     console.log("product data", data);
 
     dispatch({ type: FIND_PRODUCT_SUCCESS, payload: data });
