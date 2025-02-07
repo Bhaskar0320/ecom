e16eb5f51dba7ba73ebbd22a4bcd69e072c02cdb
Aman Bhaskar <122169164+Bhaskar0320@users.noreply.github.com>
2025-02-08 03:48:38 +0530

    original to line 42

--- src/State/Product/Action.js
+++ src/State/Product/Action.js
@@ -25,27 +25,27 @@ export const findProducts = (reqData) => async (dispatch) => {
 
   const queryParams = new URLSearchParams();
 
-  if (colors) queryParams.append("color", colors);
-  if (sizes) queryParams.append("size", sizes);
-  if (minPrice !== null && minPrice !== undefined) queryParams.append("minPrice", minPrice);
-  if (maxPrice !== null && maxPrice !== undefined) queryParams.append("maxPrice", maxPrice);
-  if (minDiscount !== null && minDiscount !== undefined) queryParams.append("minDiscount", minDiscount);
-  if (category) queryParams.append("category", category);
-  if (stock) queryParams.append("stock", stock);
-  if (sort) queryParams.append("sort", sort);
-  if (pageNumber !== null && pageNumber !== undefined) queryParams.append("pageNumber", pageNumber);
-  if (pageSize !== null && pageSize !== undefined) queryParams.append("pageSize", pageSize);  
+  // if (colors) queryParams.append("color", colors);
+  // if (sizes) queryParams.append("size", sizes);
+  // if (minPrice !== null && minPrice !== undefined) queryParams.append("minPrice", minPrice);
+  // if (maxPrice !== null && maxPrice !== undefined) queryParams.append("maxPrice", maxPrice);
+  // if (minDiscount !== null && minDiscount !== undefined) queryParams.append("minDiscount", minDiscount);
+  // if (category) queryParams.append("category", category);
+  // if (stock) queryParams.append("stock", stock);
+  // if (sort) queryParams.append("sort", sort);
+  // if (pageNumber !== null && pageNumber !== undefined) queryParams.append("pageNumber", pageNumber);
+  // if (pageSize !== null && pageSize !== undefined) queryParams.append("pageSize", pageSize);  
 
   try {
     dispatch({ type: FIND_PRODUCT_SUCCESS });
 
-//    cont {data} =       await api.get(`/api/products?color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}
-    //                    &minDiscount=${minDiscount}&category=${category}
-    //                            &stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`);
-    // console.log("product data", data)
+   cont {data} =       await api.get(`/api/products?color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}
+                       &minDiscount=${minDiscount}&category=${category}
+                               &stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`);
+    console.log("product data", data)
 
 
-    const { data } = await api.get(`/api/products?${queryParams.toString()}`);
+    // const { data } = await api.get(`/api/products?${queryParams.toString()}`);
     
     if (data && data.content && data.content.length > 0) {
       console.log("Product data:", data);
