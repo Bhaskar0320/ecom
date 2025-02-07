93221589fff72ffaf8cb29f25d2aa2cd631d7459
Aman Bhaskar <122169164+Bhaskar0320@users.noreply.github.com>
2025-02-08 03:11:09 +0530

    added request paramenter in product action line 35

--- src/State/Product/Action.js
+++ src/State/Product/Action.js
@@ -32,7 +32,7 @@ export const findProducts = (reqData) => async (dispatch) => {
     //                            &stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`);
     // console.log("product data", data)
 
-    await api.get(`/api/products?color=${colors}&size=${sizes}&minPrice=0&maxPrice=10000&minDiscount=10&category=${category}&stock=null&sort=${sort}&pageNumber=0&pageSize=12`);
+    await api.get(`/api/products?color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`);
     if (data && data.content && data.content.length > 0) {
       console.log("Product data:", data);
       dispatch({ type: FIND_PRODUCT_SUCCESS, payload: data });
